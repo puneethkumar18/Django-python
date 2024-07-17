af0f9837f5a455e39312f94b4649e48447a823be
Puneethkumar <puneethkumarg96@gmail.com>
2024-07-17 16:27:20 +0530

    django

--- demo/Todo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path("",views.index,name="index"),
-    path("list",views.todo_list,name="todo_list"),
-    path("place",views.getPalces,name="getPlaces"),
-    path("room/<int:pk>",views.room,name="room")
-]
