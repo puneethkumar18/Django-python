d59f8aa4f6266e8f964247a98399b553fcc48dae
Puneethkumar <puneethkumarg96@gmail.com>
2024-07-28 00:12:52 +0530

    refactor all after theme installations

--- studybuddy/base/views.py
+++ studybuddy/base/views.py
@@ -121,21 +121,25 @@ def userProfile(request,pk):
 @login_required(login_url='/login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid:
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect("home")
-    context = {'form':form}
-    return render(request,"base/room_form.html",context)
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name = request.POST.get('name'),
+            description = request.POST.get('description')
+        )
+        return redirect("home")
+    context = {'form':form,'topics':topics}
+    return render(request,"base/create-room.html",context)
 
 @login_required(login_url='/login')
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-    
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse('You are to allowed to update in this rooom !!')
     if request.method == "POST":
@@ -143,7 +147,7 @@ def updateRoom(request,pk):
         if form.is_valid:
             form.save()
             return redirect('home')
-    context = {'form':form}
+    context = {'form':form,'topics':topics}
     return render(request,'base/room_form.html',context)
 
 
